5c4cd6a2eab4adc1313beee306255f52c259f22e
sertanfox <39944474+sertanfox@users.noreply.github.com>
2021-03-31 13:28:15 +0200

    Sertan Versiyonu

--- lib/screens/verify_screen.dart
+++ lib/screens/verify_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:otp_screen/otp_screen.dart';
 
 class Verify_Screen extends StatefulWidget {
-  String phoneNum ;
+  String phoneNum;
   Verify_Screen({Key key, this.phoneNum}) : super(key: key);
   static String routeName = "/verify_screen";
 
@@ -14,15 +14,11 @@ class Verify_Screen extends StatefulWidget {
 }
 
 class _Verify_ScreenState extends State<Verify_Screen> {
-
-
   ///VALIDATE OTP FUTURE VOID
   //TODO: ADD FUNCTION VALIDATE OTP
 
-
   String _verificationCode;
   String _id;
-
 
   ///HERE IS THE LOGIN USER
   //TODO: ADD LOGIN USER
@@ -30,16 +26,15 @@ class _Verify_ScreenState extends State<Verify_Screen> {
   @override
   void initState() {
     super.initState();
-     ServicePhone().verifyPhone('+90${widget.phoneNum}');
+    ServicePhone().verifyPhone('+90${widget.phoneNum}');
     print('The phone number has been taken +90${widget.phoneNum}');
 
     // _loginUser();
   }
 
-
   @override
   Widget build(BuildContext context) {
-   // phoneNumber = ModalRoute.of(context).settings.arguments;
+    // phoneNumber = ModalRoute.of(context).settings.arguments;
     return _buildBodyContent(context);
 
     //   Scaffold(
@@ -62,50 +57,50 @@ class _Verify_ScreenState extends State<Verify_Screen> {
 
   Widget _buildBodyContent(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Verify Screen'),
-        elevation:0,
-      centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-                padding: EdgeInsets.only( left: 25.0, right: 25.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(hintText: 'Enter OTP'),
-                  onChanged: (val) {
-                    setState(() {
-                      this._verificationCode = val;
-                    });
-                  },
-                )),
-            Padding(
-                padding: EdgeInsets.only(left: 25.0, right: 25.0),
-                child: Container(
-                  height: 45,
-
-                  child: ElevatedButton(
-                      child: Center(
-                          child:  Text('Login',style: TextStyle(fontSize: 19),) ),
-                      onPressed: ()  {
-                        try {
-                           ServicePhone().signInWithOTP(_verificationCode, _id);
-                        } catch (e){
-                          print(' Error occured could not login ${e.toString()}');
-                        }
-
-                      }),
-                ))
-
-          ],
+        appBar: AppBar(
+          title: Text('Verify Screen'),
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.white,
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(hintText: 'Enter OTP'),
+                    onChanged: (val) {
+                      setState(() {
+                        this._verificationCode = val;
+                      });
+                    },
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(left: 25.0, right: 25.0),
+                  child: Container(
+                    height: 45,
+                    child: ElevatedButton(
+                        child: Center(
+                            child: Text(
+                          'Login',
+                          style: TextStyle(fontSize: 19),
+                        )),
+                        onPressed: () {
+                          try {
+                            ServicePhone(verificationId: _id)
+                                .signInWithOTP(_verificationCode, _id);
+                          } catch (e) {
+                            print(
+                                ' Error occured could not login ${e.toString()}');
+                          }
+                        }),
+                  ))
+            ],
+          ),
+        ));
   }
 }
-
