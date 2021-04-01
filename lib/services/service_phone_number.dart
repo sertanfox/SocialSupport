f99a7991fe16c5107afbe5f2c11a546f4834fef1
JordyHers <jordy925@gmail.com>
2021-04-01 22:05:02 +0300

    _id != null ERROR FIXED 5.Trial

--- lib/services/service_phone_number.dart
+++ lib/services/service_phone_number.dart
@@ -5,6 +5,7 @@ abstract class Service {
   Future<void> signIn(AuthCredential authCred);
   Future <void> signInWithOTP(smsCode);
   Future<void> verifyPhone(String phoneNo);
+
 }
 
 class ServicePhone extends Service {
